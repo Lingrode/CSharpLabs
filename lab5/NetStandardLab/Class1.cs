08f65b643bca52b50b6a7bc3832d96038334c902
Lingrode <grimaloilya@gmail.com>
2023-12-23 00:21:37 +0200

    completed lab5, create projects .NET Core on Linux and .NET Standard

---
+++ lab5/NetStandardLab/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace NetStandardLab
+{
+    public static class Class1
+    {
+        public static bool Validate(string email, Action Success, Action Fail)
+        {
+            bool isValidated = false;
+            string searchPattern = @"[a-zA-Z0-9_\.-]+\@[a-zA-Z0-9]+\.[a-zA-Z]+";
+            if (Regex.IsMatch(email, searchPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline))
+            {
+                Success();
+                isValidated = true;
+            }
+            else
+            {
+                Fail();
+                isValidated = false;
+            }
+            return isValidated;
+        }
+    }
+}
